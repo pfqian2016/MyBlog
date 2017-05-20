5a56f73286310f33d78bf5cb7c6fca43a4aa31f1
Pengfei Qian <qianpf0717@gmail.com>
2017-05-20 22:17:51 +0800

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+  //从服务器拉取博客简介
+  let loadBlogs = (function() {
+    $.getJSON('blogs.json', (data) => {
+      for(let i = 0; i < Math.min(data.length, 10); i++) {
+        composeBlog(data[i]);
+      }
+    });
+  })();
+  //将JSON数据组装成一条博客简介
+  function composeBlog({title, pubtime, path, abstract}) {
+    let html = '';
+    html = `<article class="article">
+      <div class="head">
+        <h1 class="title"><a href="${path}">${title}</a></h1>
+        <p class="pubtime">${pubtime}</p>
+      </div>
+      <div class="entry">
+        <blockquote>
+          <p>原文: <a href="${path}">http://www.qianpengfei.com${path}</a></p>
+        </blockquote>
+        <p class="abstract">${abstract}</p>
+        <p class="more"><a href="${path}">阅读原文&gt;&gt;</a></p>
+      </div>
+    </article>`
+    $('.article-list').append(html);
+  }
+
   let $main = $("#main");
   $("#about").click(function(event){
     event.preventDefault();
